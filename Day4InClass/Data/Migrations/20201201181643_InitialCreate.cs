5c17a22161336d859bad8b9f346557d6cb80fe47
Steven <clai86@my.bcit.ca>
2020-12-04 12:34:33 -0800

    Add project files.

---
+++ Day4InClass/Data/Migrations/20201201181643_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Day4InClass.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Description = table.Column<string>(nullable: true),
+                    IsComplete = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Description", "IsComplete" },
+                values: new object[] { 1, "Bake Cookies", false });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Description", "IsComplete" },
+                values: new object[] { 2, "Eat the cookies", false });
+
+            migrationBuilder.InsertData(
+                table: "ToDos",
+                columns: new[] { "Id", "Description", "IsComplete" },
+                values: new object[] { 3, "Run around the block", false });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
---
+++ Day4InClass/Data/Migrations/20201201190525_UpdateToDo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Day4InClass.Data.Migrations
+{
+    public partial class UpdateToDo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "ToDos",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Priority",
+                table: "ToDos",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "ToDos",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOn", "Priority" },
+                values: new object[] { new DateTime(2020, 12, 1, 11, 5, 25, 390, DateTimeKind.Local).AddTicks(9577), 1 });
+
+            migrationBuilder.UpdateData(
+                table: "ToDos",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedOn", "Priority" },
+                values: new object[] { new DateTime(2020, 12, 1, 11, 5, 25, 400, DateTimeKind.Local).AddTicks(96), 1 });
+
+            migrationBuilder.UpdateData(
+                table: "ToDos",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedOn", "Priority" },
+                values: new object[] { new DateTime(2020, 12, 1, 11, 5, 25, 400, DateTimeKind.Local).AddTicks(186), 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "ToDos");
+
+            migrationBuilder.DropColumn(
+                name: "Priority",
+                table: "ToDos");
+        }
+    }
+}
